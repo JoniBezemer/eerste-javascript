21a68d3a96acbd0e6f8ba99932769eebbf6c6ae3
JoniBezemer <77683711+JoniBezemer@users.noreply.github.com>
2021-03-23 19:18:50 +0100

    Add files via upload

---
+++ Eerste-javascript/index.js
@@ -0,0 +1,18 @@
+console.log("Hello Winc Academy")
+
+let name='Joni';
+
+//let if='if';
+//dit kan niet omdat if een gereserveerd keyword is
+
+let numbers=4+4; //ja dit kan de uitkomst is 8
+let strings='4+4'; // dit kan ook maar telt elkaar niet op de uitkomst is 4+4
+
+let addition=1+2;
+let subtraction=1-2;
+let multiplication=1*2;
+let division=1/2;
+let modulus=1%2;
+
+let mijnLeeftijd='31';
+
